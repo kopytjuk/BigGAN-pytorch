db10f6e665b7861798065cf7f5a1b43bc2c75d09
kopytjuk <kopytjuk@gmail.com>
2019-01-02 17:42:17 +0100

    update local training script.

--- start-cub.ps1
+++ start-cub.ps1
@@ -1,4 +1,4 @@
 $datafolder = "C:\Users\kopyt\Documents\DATA\cub-200-cropped"
 $experiments_path = "./experiments/1"
 
-python main.py --batch_size 64  --dataset off --adv_loss hinge --version alpha_v1 --image_path "$datafolder" --experiment_path "$experiments_path"
+python main.py --batch_size 2 --dataset off --adv_loss hinge --version alpha_v1 --image_path "$datafolder" --experiment_path "$experiments_path" --use_tensorboard True --imsize 64
